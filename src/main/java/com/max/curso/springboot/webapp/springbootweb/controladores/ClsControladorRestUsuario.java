745cec46967ed16923fcce6f4a2dba9fc5ba6c15
MaxMirManGitHub <maxmirallesmanresa@gmail.com>
2024-03-04 21:22:01 +0100

    2024-03-04 MMM cambios tras arreglo datos GIT

--- src/main/java/com/max/curso/springboot/webapp/springbootweb/controladores/ClsControladorRestUsuario.java
+++ src/main/java/com/max/curso/springboot/webapp/springbootweb/controladores/ClsControladorRestUsuario.java
@@ -16,8 +16,8 @@ public class ClsControladorRestUsuario
     @GetMapping("/Detalles2")
     public Map<String,Object> Detalles()
     {
-        //Map<String,Object> objMapBody=new HashMap<>();
-        Map<String,Object> objMapBody=new ParameterMap<>();
+        Map<String,Object> objMapBody=new HashMap<>();
+        //Map<String,Object> objMapBody=new ParameterMap<>();
 
         objMapBody.put(com.max.curso.springboot.webapp.springbootweb.ClsUtil.cnstStrTítulo1Clave,
         com.max.curso.springboot.webapp.springbootweb.ClsUtil.cnstStrTítulo1Valor);
