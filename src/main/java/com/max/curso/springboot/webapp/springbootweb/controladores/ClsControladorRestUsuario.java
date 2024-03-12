9bf4b215f498f574a51491026683f947e512dbee
MaxMirManGitHub <maxmirallesmanresa@gmail.com>
2024-03-12 22:48:28 +0100

    2024-03-12 MMM faltaba @RestController en el controlador rest

--- src/main/java/com/max/curso/springboot/webapp/springbootweb/controladores/ClsControladorRestUsuario.java
+++ src/main/java/com/max/curso/springboot/webapp/springbootweb/controladores/ClsControladorRestUsuario.java
@@ -9,15 +9,21 @@ import org.apache.catalina.util.ParameterMap;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.ResponseBody;
-//import org.springframework.web.bind.annotation.RestController;
-//import org.springframework.web.bind.annotation.RequestMethod;
-//import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/*
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+*/
 
 import com.max.curso.springboot.webapp.springbootweb.ClsUtil;
 
 //Controller + ResponseBody equivale a RestController
 //@Controller 
+
+@RestController
 
 @RequestMapping("/api")
 
